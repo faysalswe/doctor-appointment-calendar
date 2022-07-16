0370d9f7c3d139b93ab4178e937aa16a82ef70ee
Faysal Ahmmad <faysalswe@gmail.com>
2022-07-16 20:10:54 +0600

    ADD: angular material added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppointmentCalendarComponent } from './appointment-calendar/appointment-calendar.component';
 import { NewAppointmentComponent } from './new-appointment/new-appointment.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [		
@@ -14,7 +15,8 @@ import { NewAppointmentComponent } from './new-appointment/new-appointment.compo
    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
