61cbdb58d000680e5382876cad93640b1ffdba9e
Faysal Ahmmad <faysalswe@gmail.com>
2022-07-17 14:21:50 +0600

    ADD: routing added by month

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppointmentCalendarComponent } from './pages/appointment-calendar/appointment-calendar.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'month/:month',
+    pathMatch: 'full'
+  },
+  {
+    path: 'month/:month',
+    component: AppointmentCalendarComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
